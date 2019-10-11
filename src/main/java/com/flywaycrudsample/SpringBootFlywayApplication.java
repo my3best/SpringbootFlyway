8fc5eb3d6474e5683e068d6523b9873a4520a895
MounikaDudyala <mounikadudyala1998@gmail.com>
2019-10-11 17:05:04 +0530

    added jackson dependency

--- src/main/java/com/flywaycrudsample/SpringBootFlywayApplication.java
+++ src/main/java/com/flywaycrudsample/SpringBootFlywayApplication.java
@@ -2,7 +2,7 @@ package com.flywaycrudsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.flywaycrudsample"})
 public class SpringBootFlywayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFlywayApplication.class, args);
